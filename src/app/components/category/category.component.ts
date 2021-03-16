5f78427384dd407c714a74edeca4543b7ec88b11
Berke <berkealm@gmail.com>
2021-03-16 19:29:18 +0300

    frontend

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  categories:Category[] = [];
+  dataLoaded:boolean=false;
+
+  currentCategory:Category | null;
+
+
+  constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
   }
 
+  getCategories(){
+    this.categoryService.getCategories().subscribe((response) => {
+      this.categories = response.data;
+      this.dataLoaded = true;
+    });
+  }
+
+  setCurrentCategory(category:Category){
+      this.currentCategory = category;
+  }
+
+  setNullCategory(){
+    this.currentCategory = null;
+  }
+
+  getCurrentCategoryClass(category:Category | null){
+    if(category == this.currentCategory){
+      return "list-group-item active"
+    }
+    else{
+      return "list-group-item"
+    }
+  }
 }
